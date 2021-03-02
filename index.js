69d2bfd1b4256adea14048970abeb9a99e1a6580
Alan Alves <alanalves071@gmail.com>
2021-03-02 14:24:23 -0300

    Adicionando Variaveis e Operadores

---
+++ index.js
@@ -0,0 +1,21 @@
+/* VARIAVEIS */
+var numero = 1;
+var string = 'a';
+var bolean = true;
+var objeto = {
+    nome: 'alan',
+    idade: '20'
+};
+
+console.log(typeof numero, numero);
+console.log(typeof string, string);
+console.log(typeof bolean, bolean);
+console.log(typeof objeto, objeto)
+
+/* OPERADORES*/
+/* '+' = SOMA/CONCATENAÇÃO*/
+var n1 = 5;
+var n2 = 3;
+
+var soma = n1 + n2;
+console.log(n1, n2, soma)
