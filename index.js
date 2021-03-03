0c0fdac9aeefb2ea7055769a0c6ca160606f5125
Alan Alves <alanalves071@gmail.com>
2021-03-03 10:21:29 -0300

    Adicionando o LOOP

--- index.js
+++ index.js
@@ -76,4 +76,28 @@ if (pessoa.altura > 1.70){
 }else {
     console.log(`${pessoa.nome} tem menos de 1.56 então é pequeno`)
 }
+console.log('----------------------LOOPS-------------------------------')
 
+var pessoas = [{
+    nome: 'alan',
+    idade: '20'
+},{
+    nome: 'lucas',
+    idade: '15',
+},{
+    nome: 'domi',
+    idade: '13'
+}
+]
+
+var funcaoLoop = function(){
+    for (i = 0; i < pessoas.length; i++){
+        var maiorDe = pessoas[i]
+        if(maiorDe.idade < 18){
+            console.log(`${maiorDe.nome} ele tem ${maiorDe.idade} então é menor de idade`)
+        }else if (maiorDe.idade >= 18){
+            console.log(`${maiorDe.nome} ele tem ${maiorDe.idade} então é maior de idade`)}
+
+    }}
+
+funcaoLoop()
